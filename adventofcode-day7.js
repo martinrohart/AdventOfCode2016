4b4d9c48f93e681bc0a69cc4059de8e316bcb438
Martin Rohart <martin.rohart@istone.com>
2016-12-08 20:26:59 +0100

    Day 7+8

---
+++ adventofcode-day7.js
@@ -0,0 +1,110 @@
+﻿var fs = require('fs');
+var puzzle = 1;
+
+fs.readFile('input-day7.txt', (err, stream) => {
+    main(stream.toString());
+});
+
+
+function main(input){
+	var countTLS = 0;
+	var countSSL = 0;
+    var lines = input.split("\n");
+    for(var line of lines){
+    	if(supportsTLS(line)){
+    		countTLS++;
+    	}
+    	if(supportsSSL(line)){
+    		countSSL++;
+    	}
+    }	
+
+    console.log("IPs with TLS: "+countTLS+" of "+lines.length);
+    console.log("IPs with SSL: "+countSSL+" of "+lines.length);
+}
+
+function supportsSSL(line)
+{
+	var tokens = line.split(']');
+
+	var ABAs = [];
+	var hypernets = [];
+
+	var regex = /^\s*([a-z]+)(\[([a-z]+))?\s*$/
+
+	for(var token of tokens){
+		var r = token.match(regex);
+	    if(r!=null && r.length>1){
+	    	if(r[3]){
+	    		hypernets.push(r[3]);
+	    	}
+	    	var result =getABAs(r[1]);
+	    	for(var aba of result){
+	    		ABAs.push(aba);
+	    	}
+	    }
+	}
+
+	for(var hypernet of hypernets){
+		for(var aba of ABAs){
+			if(hasBAB(aba.substring(0,1), aba.substring(1,2),hypernet)){
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+function getABAs(text)
+{
+	var result = [];
+	var array = text.split('');
+	for(var i=0;i<(array.length-2);i++){
+		if(array[i]!=array[i+1] && array[i]==array[i+2]){
+			result.push(array[i]+array[i+1]+array[i+2]);
+		}
+	}
+	return result;
+}
+
+function hasBAB(a, b, text)
+{
+	//console.log("hasBAB: "+a+", "+b+" in "+text);
+	var array = text.split('');
+	for(var i=0;i<(array.length-2);i++){
+		if(array[i]==b && array[i+1]==a && array[i+2]==b){
+			return true;
+		}
+	}
+	return false;
+}
+
+function supportsTLS(line)
+{
+	var success = false;
+	var tokens = line.split(']');
+	for(var token of tokens){
+
+		var regex = /^\s*([a-z]+)(\[([a-z]+))?\s*$/
+		var r = token.match(regex);
+	    if(r!=null && r.length>1){
+	    	if(r[3] && hasABBA(r[3])){
+	    		return false;
+	    	}
+	    	success = success || hasABBA(r[1]);
+	    }
+	}
+	return success;
+}
+
+function hasABBA(text)
+{
+	var array = text.split('');
+	for(var i=0;i<(array.length-3);i++){
+		if(array[i]!=array[i+1] && array[i+1]==array[i+2] && array[i]==array[i+3]){
+			return true;
+		}
+	}
+	return false;
+}
---
+++ adventofcode-day8.js
@@ -0,0 +1,103 @@
+﻿var fs = require('fs');
+
+var screen = [ 	[],
+					[],
+					[],
+					[],
+					[],
+					[]];
+
+
+fs.readFile('input-day8.txt', (err, stream) => {
+    main(stream.toString());
+});
+
+
+function main(input){
+	screen = screen.map(function(value){
+		for (var i=0;i<50;i++){
+			value.push('.');
+		}
+		return value;
+	});
+
+    var lines = input.split("\n");
+    for(var line of lines){
+
+    	if(line.indexOf("rect")==0){
+    		var regex = /^\s*rect\s(\d+)x(\d+)\s*$/;
+    		var r = line.match(regex);
+		    if(r!=null && r.length==3){
+		    	var height = parseInt(r[2]);
+		    	var width = parseInt(r[1]);
+		    	for(var i = 0; i<height;i++){
+		    		for(var j=0;j<width;j++){
+						screen[i][j] = 1;
+					}
+		    	}
+	    	}
+	    }else if(line.indexOf("rotate row")==0){
+    		var regex = /^\s*rotate\srow\sy=(\d+)\sby\s(\d+)\s*$/;
+    		var r = line.match(regex);
+		    if(r!=null && r.length==3){
+		    	var rowIndex = parseInt(r[1]);
+		    	var offset = parseInt(r[2]);
+		    	screen[rowIndex] = screen[rowIndex].map(function(value,index,self){
+		    		if((index-offset)<0){
+		    			return self[self.length-(offset-index)];
+		    		}else{
+		    			return self[index-offset]
+		    		}
+		    	});
+	    	}
+	    }else if(line.indexOf("rotate column")==0){
+    		var regex = /^\s*rotate\scolumn\sx=(\d+)\sby\s(\d+)\s*$/;
+    		var r = line.match(regex);
+		    if(r!=null && r.length==3){
+		    	var columnIndex = parseInt(r[1]);
+		    	var offset = parseInt(r[2]);
+
+		    	var column = [screen[0][columnIndex],screen[1][columnIndex],screen[2][columnIndex],screen[3][columnIndex],screen[4][columnIndex],screen[5][columnIndex] ];
+		    	column = column.map(function(value,index,self){
+		    		if((index-offset)<0){
+		    			return self[self.length-(offset-index)];
+		    		}else{
+		    			return self[index-offset]
+		    		}
+		    	});
+		    	screen[0][columnIndex] = column[0];
+		    	screen[1][columnIndex] = column[1];
+		    	screen[2][columnIndex] = column[2];
+		    	screen[3][columnIndex] = column[3];
+		    	screen[4][columnIndex] = column[4];
+		    	screen[5][columnIndex] = column[5];
+	    	}
+	    }
+
+    	displayScreen();
+    }	
+
+    console.log("Pixels "+pixelsLit());
+}
+
+function pixelsLit()
+{
+	var count = 0;
+	for(var row of screen){
+		for(var i=0;i<row.length;i++){
+			if(row[i]==1){
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+function displayScreen()
+{
+	console.log("Screen starts#########################");
+	for (var line of screen){
+		console.log(line.join(''));
+	}
+	console.log("Screen ends#########################");
+}
